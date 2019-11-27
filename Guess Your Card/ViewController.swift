f2b5f5f43f44ad67c57a9904561083b22607fe63
Yasin Shamrat <yshamrat@gmail.com>
2019-11-27 14:07:02 +0600

    Initial Commit

---
+++ Guess Your Card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess Your Card
+//
+//  Created by Leads Computer on 11/27/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
